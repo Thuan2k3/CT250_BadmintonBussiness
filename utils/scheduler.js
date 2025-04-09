1c400089fae3fb5ed7e7496962943039f611f580
THUAN\tranm <thuant1002@gmail.com>
2025-04-09 10:58:54 +0700

    node-cron theo gio Viet Nam

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -2,8 +2,14 @@ const cron = require("node-cron");
 const updateNoShowAndReputation = require("./updateNoShow");
 
 // Lên lịch chạy hàm updateNoShowAndReputation lúc 01:00 mỗi ngày
-cron.schedule("0 1 * * *", async () => {
-  console.log("🔔 Đang kiểm tra và cập nhật trạng thái no-show...");
-  await updateNoShowAndReputation();
-});
+cron.schedule(
+  "0 1 * * *",
+  async () => {
+    console.log("🔔 Đang kiểm tra và cập nhật trạng thái no-show...");
+    await updateNoShowAndReputation();
+  },
+  {
+    timezone: "Asia/Ho_Chi_Minh", // ✅ Thêm timezone Việt Nam
+  }
+);
 updateNoShowAndReputation();
