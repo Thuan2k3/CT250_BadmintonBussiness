8ff3f0d903821e498e5ef744068e66c30f72b195
THUAN\tranm <thuant1002@gmail.com>
2025-04-10 15:13:02 +0700

    An cac file can thiet

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,25 @@
+const dotenv = require("dotenv");
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json"); // Đường dẫn tới file JSON
+
+dotenv.config();
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://badmintoncourtbussiness-default-rtdb.firebaseio.com/", // Thay bằng URL Firebase của bạn
+  databaseURL: process.env.FIREBASE_DB_URL,
 });
 
 const db = admin.database();
